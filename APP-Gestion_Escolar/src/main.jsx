e77aa325f5851869e74f5cfb7db9f45d44271623
MatiasMXS <matifmorenoxd@gmail.com>
2025-02-05 00:52:03 -0300

    Fix2 para deployar en vercel

--- APP-Gestion_Escolar/src/main.jsx
+++ APP-Gestion_Escolar/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import store from "./store/store.jsx";
+import store from "./Store/store.jsx";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
